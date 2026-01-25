1b92f460f1cc61e0acd0023728b23da28a915849
Marco Demartino <marcodemartino23@gmail.com>
2026-01-25 22:37:20 +0100

    feat(sidebar): Add sidebar with platform filter

---
+++ src/lib/components/ui/app-sidebar/app-sidebar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+import MultiSelect from '@/components/ui/multi-select/multi-select.svelte';
+
+let { platformNames, selectedPlatforms = $bindable() } = $props();
+</script>
+
+<Sidebar.Root class="">
+	<Sidebar.Content class="p-4">
+		<button class="sr-only" type="button">Sidebar opened</button>
+		<Sidebar.GroupLabel>Filter</Sidebar.GroupLabel>
+		<Sidebar.Menu>
+			<Sidebar.MenuItem>
+				<MultiSelect platformNames={platformNames} bind:selectedPlatforms={selectedPlatforms} />
+			</Sidebar.MenuItem>
+		</Sidebar.Menu>
+	</Sidebar.Content>
+</Sidebar.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,8 @@
 	import { _extractPlatformNames, _fetchDepartures, _filterByPlatformName } from './+page';
 	import { formatTime } from '@/utils';
 	import MultiSelect from '@/components/ui/multi-select/multi-select.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppSidebar from '@/components/ui/app-sidebar/app-sidebar.svelte';
 
 	const { data }: PageProps = $props();
 
@@ -19,7 +21,7 @@
 	let departuresToShow = $derived(_filterByPlatformName(departures, selectedPlatforms));
 	const platformNames = $derived(_extractPlatformNames(departures));
 
-	let stationId = $derived(page.url.searchParams.get('stationId') || '7000090');
+	let stationId = $derived(page.url.searchParams.get('stationId') || 'de:08212:89');
 	let stationName = $derived(departures.stationName);
 
 	async function fetchAndSetDepartures(stationId: string) {
@@ -41,36 +43,46 @@
 		clearInterval(depTimer);
 		clearInterval(clockTimer);
 	});
+	let sidebarOpen = $state(false);
 </script>
 
 <svelte:head>
 	<title>{stationName}</title>
 </svelte:head>
 
-<div class="bg-[#c30a37] flex justify-between items-center p-4 gap-4">
-	<div class="flex justify-between gap-4 flex-wrap w-full min-w-0">
-		<SearchBar bind:selectedId={stationId} bind:selectedValue={stationName} />
-		<MultiSelect {platformNames} bind:selectedPlatforms />
-		<div></div>
-	</div>
+<Sidebar.Provider
+	bind:open={sidebarOpen}
+	style="--sidebar-width: 17rem; --sidebar-width-mobile:  fit-content;"
+>
+	<AppSidebar platformNames={platformNames} bind:selectedPlatforms={selectedPlatforms} />
+	<main class="w-full">
+		<div class="flex items-center justify-between gap-4 bg-[#c30a37] p-4">
+			<div class="flex w-full min-w-0 flex-wrap justify-between gap-4">
+				<SearchBar bind:selectedId={stationId} bind:selectedValue={stationName} />
+				<div></div>
+			</div>
 
-	<p class="text-white font-medium">{time}</p>
-</div>
+			<p class="font-medium text-white">{time}</p>
+			<Sidebar.Trigger class="text-white"/>
 
-<div class="p-4">
-	{#if !departures}
-		<p>Loading data...</p>
-	{:else if departures.stationName === ''}
-		<p>Error: The id {stationId} is invalid.</p>
-	{:else if departuresToShow.length === 0}
-		<p>No departures found for the selected stop.</p>
-	{:else}
-		<div class="flex flex-col gap-2">
-			{#key departuresToShow}
-				{#each departuresToShow as departure}
-					<DepartureInfo cityName={departures.cityName} {departure} />
-				{/each}
-			{/key}
 		</div>
-	{/if}
-</div>
+
+		<div class="p-4">
+			{#if !departures}
+				<p>Loading data...</p>
+			{:else if departures.stationName === ''}
+				<p>Error: The id {stationId} is invalid.</p>
+			{:else if departuresToShow.length === 0}
+				<p>No departures found for the selected stop.</p>
+			{:else}
+				<div class="flex flex-col gap-2">
+					{#key departuresToShow}
+						{#each departuresToShow as departure}
+							<DepartureInfo cityName={departures.cityName} departure={departure} />
+						{/each}
+					{/key}
+				</div>
+			{/if}
+		</div>
+	</main>
+</Sidebar.Provider>
